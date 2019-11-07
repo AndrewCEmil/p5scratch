896fc2ca477220440fd22af4c966d7feed697926
ace <ace@0xace.com>
2019-11-06 21:46:44 -0500

    Snake line, looks really cool

---
+++ snake_line.js
@@ -0,0 +1,24 @@
+let width = 1600;
+let height = 1100;
+var x_points = 25;
+var y_points = 25;
+var point_width = 50;
+var random_width = 30;
+var radius = 10;
+var grid = [];
+var new_grid = [];
+var step = 0;
+function setup() {
+  createCanvas(width, height, WEBGL);
+}
+
+function draw() {
+  clear();
+  translate(-width/3,-height/4,0)
+  var num_boxes = 20;
+  for (var i = 0; i < num_boxes; i++) {
+    rotateX(millis() / 1000);
+    box(50, 50, 50);
+    translate(width/(num_boxes),height/(num_boxes),0)
+  }
+}
